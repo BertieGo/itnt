899330d0b720069b3c9ab289b92ac8c2c8703df8
huang wei <huangwei@coding.net>
2020-03-27 18:17:15 +0800

    remove hardcode

--- index.js
+++ index.js
@@ -3,6 +3,5 @@ const path = require('path');
 const util = require('./util');
 const { REG } = require('./constants');
 
-util.entry('/Users/huangwei/workspace/coding-private-frontend/coding-frontend/coding-front-v2/packages/share', './');
 
-// util.entry('./test', './');
+util.entry('./test', './');
